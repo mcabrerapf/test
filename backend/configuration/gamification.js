a6d30b45568f0f3089aad0feedc53471f16e40bd
BTISAVB\BTIS <blai.celma@basetis.com>
2016-12-05 21:57:22 +0100

    first commit

--- backend/configuration/gamification.js
+++ backend/configuration/gamification.js
@@ -1,18 +1,17 @@
-
 exports.configuration = {
-    wwwPort:    8090,
-    mongoDb:    'mongodb://office.kaldeera.com:38518/gamification',
-//    mongoDb:    'mongodb://debian:27017/gamification',
-    apiUrl:     '/api'
+  wwwPort: 8090,
+  // mongoDb:    'mongodb://office.kaldeera.com:38518/gamification',
+  mongoDb: 'mongodb://localhost:27017/gamification',
+  apiUrl: '/api'
 };
 
 exports.accessLevels = {
-    'public':       '*',
-    'anonymous':    ['public'],
-    'editor':       ['editor', 'admin'],
-    'admin':        ['admin']
+  'public': '*',
+  'anonymous': ['public'],
+  'editor': ['editor', 'admin'],
+  'admin': ['admin']
 };
 
 exports.paths = {
-    dataDir:        '../../../data'
+  dataDir: '../../../data'
 }
